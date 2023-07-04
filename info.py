26534c2ace3811d18b8f47afb2091447c2c81406
publicGH <136681502+publicGH@users.noreply.github.com>
2023-07-04 19:17:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,6 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-REQ_CHANNEL = environ.get("REQ_CHANNEL", True)
-REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://salamanco:salamanco@cluster0.cepnjcq.mongodb.net/?retryWrites=true&w=majority")
